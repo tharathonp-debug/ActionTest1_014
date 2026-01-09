6ec160252331697f145a1ad4b8f4d61f42235b16
tharathonp-debug <tharathon.p@rmutsvmail.com>
2026-01-09 11:43:01 +0700

    Update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,21 +1,5 @@
-<?php
+from main import add
 
-use PHPUnit\Framework\TestCase;
-use App\Calculator;
-
-class CalculatorTest extends TestCase
-{
-    public function testAdd()
-    {
-        $calc = new Calculator();
-        $this->assertEquals(5, $calc->add(2, 3));
-        $this->assertEquals(0, $calc->add(-1, 1));
-    }
-
-    public function testSubtract()
-    {
-        $calc = new Calculator();
-        $this->assertEquals(1, $calc->subtract(3, 2));
-        $this->assertEquals(-2, $calc->subtract(-1, 1));
-    }
-}
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 3) == 0
